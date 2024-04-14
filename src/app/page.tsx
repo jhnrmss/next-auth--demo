cefd69b6c5b36551ada01327ca5e57e5e40bf3bc
John Romie <johnromie37@gmail.com>
2024-04-14 11:20:01 +0800

    chore/add components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import AppButton from "./_components/AppButton/page";
+import AppButton from "./_components/AppButton";
 
 export default function Home() {
   return (
